f56feb2cd9644df85c92f88265773605de6361a2
Sam Cheng <mariaach@usc.edu>
2023-08-05 23:28:58 -0700

    add socials logos

--- site/src/components/NavBar.jsx
+++ site/src/components/NavBar.jsx
@@ -6,25 +6,28 @@ import '../styles/sam/NavBar.css';
 
 function Header () {
     return (
-        <Navbar bg="light" expand="md" className="navbar">
-          <Container fluid>
-            <Nav className="page-links">
-              <Nav.Link href="#about">About</Nav.Link>
-              <Nav.Link href="#events">Events</Nav.Link>
-              <Nav.Link href="#contact">Contact Us</Nav.Link>
-            </Nav>
+        <>
+          <Navbar bg="light" expand="md" className="navbar">
+            <Container fluid>
+              <Nav className="page-links">
+                <Nav.Link href="#about">About</Nav.Link>
+                <Nav.Link href="#events">Events</Nav.Link>
+                <Nav.Link href="#contact">Contact Us</Nav.Link>
+              </Nav>
 
-            <Navbar.Brand  className="nav-logo">
-              <img src={saseLogo} alt="sase logo" className="nav-logo"/>
-            </Navbar.Brand>
+              <Navbar.Brand  className="nav-logo">
+                <img src={saseLogo} alt="sase logo" className="nav-logo"/>
+              </Navbar.Brand>
 
-            <Nav className="social-links">
-              <Nav.Link href="#facebook">Facebook</Nav.Link>
-              <Nav.Link href="#twitter">Twitter</Nav.Link>
-              <Nav.Link href="#instagram">Instagram</Nav.Link>
-            </Nav>
-          </Container>
-        </Navbar>
+              <Nav className="social-links">
+                <Nav.Link href="#facebook">Facebook</Nav.Link>
+                <Nav.Link href="#twitter">Twitter</Nav.Link>
+                <Nav.Link href="#instagram">Instagram</Nav.Link>
+              </Nav>
+            </Container>
+          </Navbar>
+          <p>hi</p>
+        </>
       );
 }
 
