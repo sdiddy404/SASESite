faf316a8543ae8b567c8098db24abdb1cae8d6c1
Mike Gee <mpgee@usc.edu>
2023-08-09 18:07:01 -0700

    style changes

--- site/src/components/events/FeaturedEvent.jsx
+++ site/src/components/events/FeaturedEvent.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function FeaturedEvent(props) {
   return (
     <div>
-      <img src={props.img} alt="WRC 2022 pic" className="featured-event--image" width="500em"></img>
+      <img src={props.img} alt="WRC 2022 pic1100 Exposition Blvd Unit 2" className="featured-event--image" width="500em"></img>
       <div className="featured-event--text">
         <h1>{props.header}</h1>
         <p>{props.subheader}</p>
--- site/src/components/events/UpcomingEvents.jsx
+++ site/src/components/events/UpcomingEvents.jsx
@@ -4,11 +4,11 @@ import { InstagramEmbed } from 'react-social-media-embed';
 export default function UpcomingEvents() {
   return (
     <div>
+      <InstagramEmbed className="upcoming-events--instagram" url="https://www.instagram.com/p/CreoZmjPuHe/?utm_source=ig_web_button_share_sheet&igshid=MzRlODBiNWFlZA==" width={328} height={424}/>
+      <div className='upcoming-events--text'>
         <h1 className='upcoming-events--header'>Upcoming Events</h1>
         <p className='upcoming-events--subheader'>Kick of the semester right and get to know us by speaking with current board members to learn more SASE!</p>
-        <div style={{ display: 'flex', justifyContent: 'center' }}>
-          <InstagramEmbed url="https://www.instagram.com/p/CreoZmjPuHe/?utm_source=ig_web_button_share_sheet&igshid=MzRlODBiNWFlZA==" width={328} />
-        </div>
+      </div>
     </div>
   )
 }
