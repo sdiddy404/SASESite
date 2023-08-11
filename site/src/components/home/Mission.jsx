c027e7f45e5875a9bc4a965bb043ab16731fe0a5
Mike Gee <mpgee@usc.edu>
2023-08-10 17:19:29 -0700

    Revert "style changes (might redo css for home page)"
    This reverts commit eb5cfef2f27c07685ae555c71236c9cdf5e45ce2.

--- site/src/components/home/Mission.jsx
+++ site/src/components/home/Mission.jsx
@@ -9,7 +9,7 @@ export default function Mission() {
     return (
     <div className="mission">
         <MissionStatement/>
-        <div className="mission-item-container">
+        <div className="mission--mission-item-container">
             <MissionItem className="mission-item mission--item--1" img={Handshake} text="Prepare Asian heritage scientists and engineers for success in the global business world" />
             <MissionItem className="mission-item mission--item--2" img={Diversity} text="Celebrate diversity on campuses and in the workplace" />
             <MissionItem className="mission-item mission--item--3" img={Group} text="Provide opportunities for members to make contributions to their local communities" />
