d62dd488b3c6f0d95dc827c706e2fbe353e45250
Mike Gee <mpgee@usc.edu>
2023-08-10 17:44:41 -0700

    reorganized css

--- site/src/components/home/MissionStatement.jsx
+++ site/src/components/home/MissionStatement.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Mission() {
   return (
-    <div className="mission--statement">
+    <div className="mission-statement">
         <h1><strong>Our mission</strong></h1>
         <p>SASE at USC is dedicated to cultivating, promoting, and advocating for Asian heritage leadership in science and engineering in school and the workforce.</p>
     </div>
