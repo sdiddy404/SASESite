34cb0c60b233c5512a9bd25b38985c4a5407aa05
Mike Gee <mpgee@usc.edu>
2023-08-10 18:22:34 -0700

    this css is GOLDEN

--- site/src/components/home/Mission.jsx
+++ site/src/components/home/Mission.jsx
@@ -8,7 +8,9 @@ import Group from "../../images/home/group.png"
 export default function Mission() {
     return (
     <div className="mission">
-        <MissionStatement/>
+        <div className='mission-statement-container'>
+            <MissionStatement/>
+        </div>
         <div className="mission-item-container">
             <MissionItem className="mission-item mission--item-1" img={Handshake} alt="Handshake" text="Prepare Asian heritage scientists and engineers for success in the global business world" />
             <MissionItem className="mission-item mission--item-2" img={Diversity} alt="Diversity" text="Celebrate diversity on campuses and in the workplace" />
