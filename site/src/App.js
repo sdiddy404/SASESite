ede32618a3dfe9fe929057f38503e4c8fdd28270
Mike Gee <mpgee@usc.edu>
2023-08-10 18:11:54 -0700

    Revert "idk what i did tbh"
    This reverts commit 49d7caed1ac245d41d8ae035468e87f2e4a4586f.

--- site/src/App.js
+++ site/src/App.js
@@ -1,3 +1,7 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from "./pages/home"
 import Events from "./pages/events"
 
