8c4985b7f12731829b429df051caaa675e7da52c
Sam Cheng <mariaach@usc.edu>
2023-08-21 13:57:20 -0700

    conflict issues

--- site/src/App.js
+++ site/src/App.js
@@ -1,19 +1,34 @@
 // import logo from './logo.svg';
 // import './App.css';
-import About from './pages/About';
-import NavBar from './components/NavBar';
-import Footer from "./components/Footer";
-import Contact from './pages/Contact';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import About from './pages/About';
+// import NavBar from './components/NavBar';
+// import Footer from "./components/Footer";
+// import Contact from './pages/Contact';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+// function App() {
+//   return (
+//     <>
+//       <NavBar />
+//       {/* <About /> */}
+//       <Contact />
+//       <Footer />
+//     </>
+//   );
+// }
+
+// export default App;
+
+
+import logo from './logo.svg';
+import Button from 'react-bootstrap/Button';
+import Home from "./pages/home"
+import Events from "./pages/events"
 
 function App() {
   return (
-    <>
-      <NavBar />
-      {/* <About /> */}
-      <Contact />
-      <Footer />
-    </>
+    <Home />
+    // <Events />
   );
 }
 
